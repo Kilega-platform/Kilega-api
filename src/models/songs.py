e38da6fbe1e0f82bc12bc5988e6488714196a161
danielogen <danielogen@gmail.com>
2021-02-24 10:49:24 +0300

    add songs model

---
+++ src/models/songs.py
@@ -0,0 +1,17 @@
+from config import db_connection
+
+
+class Songs():
+    """This class contains functionality for dealing with songs
+    Attributes:
+    """
+
+    def __init__(self, lang):
+        """ initialize """
+        self.lang = lang
+
+    def get_all_songs(self):
+        """ get all songs from the songs collection """
+        db = db_connection.connect_mongo(self.lang)
+        results = list(db.songs.find({}))
+        return results
