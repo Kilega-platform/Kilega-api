709a5c1b1335da88f7e00abc867089857d22433b
danielogen <danielogen@gmail.com>
2021-03-01 11:01:45 +0300

    send emails with smtplib

---
+++ src/helpers/sendmail.py
@@ -0,0 +1,24 @@
+import smtplib
+from config.db_connection import app_configuration
+from dotenv import load_dotenv
+from email.message import EmailMessage
+load_dotenv()
+
+MAIL_USERNAME = app_configuration.MAIL_USERNAME
+MAIL_PASSWORD = app_configuration.MAIL_PASSWORD
+MAIL_SERVER = app_configuration.MAIL_SERVER
+MAIL_PORT = app_configuration.MAIL_PORT
+
+
+def send_email(subject, sender, recipients, body):
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipients
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(body)
+
+    with smtplib.SMTP_SSL(MAIL_SERVER, MAIL_PORT) as smtp:
+        smtp.login(MAIL_USERNAME, MAIL_PASSWORD)
+
+        smtp.send_message(msg)
