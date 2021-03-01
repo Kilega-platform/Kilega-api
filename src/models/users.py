9d4832eee51d8bc93c9ef5c03b62b1874da42a7f
danielogen <danielogen@gmail.com>
2021-03-01 11:02:05 +0300

    user model

---
+++ src/models/users.py
@@ -0,0 +1,37 @@
+import sys
+import bcrypt
+from config import db_connection
+from pymongo.errors import OperationFailure
+
+
+class User():
+    def __init__(self, lang):
+        self.lang = lang
+
+    def register_user(self, record):
+        db = db_connection.connect_mongo(self.lang)
+        result = db.users.insert_one(record)
+        return result
+
+    def hash_password(self, password):
+        return bcrypt.hashpw(password, bcrypt.gensalt())
+
+    # check if user exist
+
+    def is_user_exist(self, email):
+        db = db_connection.connect_mongo(self.lang)
+        result = db.users.find_one({"email": email})
+        return result
+
+    def login_user(self, email, password):
+        db = db_connection.connect_mongo(self.lang)
+        result = db.users.find_one({"email": email})
+        if result:
+            stored_password = result["password"]
+            if bcrypt.hashpw(password, stored_password) == stored_password:
+                return result
+        return False
+
+    def get_users(self):
+        db = db_connection.connect_mongo(self.lang)
+        return list(db.users.find({}, {"email": 1, "privilege": 1, "userName": 1, "_id": 0}))
