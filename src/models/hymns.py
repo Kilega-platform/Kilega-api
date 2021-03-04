21af508a7d8baa09107f9855e7cd92b7e873619b
danielogen <danielogen@gmail.com>
2021-03-05 00:06:48 +0300

    query hymns based on language

--- src/models/hymns.py
+++ src/models/hymns.py
@@ -13,5 +13,8 @@ class Hymns():
     def get_all_hymns(self):
         """ get all songs from the songs collection """
         db = db_connection.connect_mongo()
-        results = list(db.hymns.find({"lang": {"eq": self.lang}}, {"_id": 0}))
+        query = {"lang": {"$eq": self.lang}}
+        projection = {"_id": 0, "song_number": 1,
+                      "verses": 1, "category": 1, "title": 1, "lang": 1}
+        results = list(db.hymns.find(query, projection))
         return results
