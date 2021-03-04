4898781a3869df5c0cc1f7658aa057d35e1b5214
danielogen <danielogen@gmail.com>
2021-03-04 23:06:54 +0300

    remove db url from health check

--- src/controllers/check_health.py
+++ src/controllers/check_health.py
@@ -7,6 +7,7 @@ import json
 from datetime import datetime, timedelta
 from routes import api
 import os
+from http import HTTPStatus
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -19,11 +20,11 @@ monitor_bp = Blueprint('monitor_bp', __name__)
 def root():
     if request.method == 'GET':
         _logger.info('root endpoint OK')
-        return jsonify({"message": "ok", "status": True}), 200
+        return jsonify({"message": "ok", "status": True}), HTTPStatus.ACCEPTED
 
 
 @monitor_bp.route(api.route['health_check'], methods=['GET', 'POST'])
 def health():
     if request.method == 'GET':
         _logger.info('health status OK')
-        return jsonify({"message": f'health check passed. DB_URL: {app_configuration.MONGO_URI}, DB_NAME: {app_configuration.DB_NAME}', "status": True}), 200
+        return jsonify({"message": "ok", "status": True}), HTTPStatus.ACCEPTED
